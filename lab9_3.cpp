1309cc8727945579093967a3a4a5df9a9a159466
phum <phumrapee.phum@gmail.com>
2022-12-23 16:17:15 +0700

    lab9_3complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,38 @@
 "Brook"
 "Jinbe"
 */
+
+#include<iostream>
+using namespace std;
+int main(){
+  int age, h, b;  
+  cout << "Enter your age: ";
+  cin >> age;
+  if(age > 25){
+    if(age > 60){
+      cout << "Your character = Brook";
+      return 0;
+    }
+    cout << "Enter your bounty: ";
+    cin >> b;
+    if(b > 500000000){
+      cout << "Your character = Jinbe";
+      return 0;
+    } cout << "Your character = Franky";
+    return 0;
+  }
+  cout << "Enter your height: ";
+  cin >> h;
+  if(h < 100){ cout << "Your character = Chopper";
+    return 0;
+    }
+  else if(h < 180){ cout << "Your character = Usopp";
+    return 0;
+    }
+  cout << "Enter your bounty: ";
+  cin >> b;
+  if(b > 1100000000) cout << "Your character = Zoro";
+  else cout << "Your character = Sanji";
+
+  return 0;
+}
